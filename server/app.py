8068baa441a9f497daa14148ba131421dcd6b400
johnkagiri <kagirijohn124@gmail.com>
2023-11-23 12:57:12 +0300

    first commit

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request, make_response, session
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+api= Api(app)
+
+class Home(Resource):
+    def get(self):
+
+        return {'message': 'Username already in use. Please choose a different one.'},200
+    
+api.add_resource(Home, '/',endpoint='home')    
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
